d6b947d3532c05789fa72ae0501e2736c05e65c7
Freddy Kristiansen <freddy.kristiansen@microsoft.com>
2017-10-11 06:00:20 +0200

    Add Install-VS2017

---
+++ Install-VS2017Community.ps1
@@ -0,0 +1,23 @@
+﻿if (!(Test-Path function:Log)) {
+    function Log([string]$line, [string]$color = "Gray") {
+        ("<font color=""$color"">" + [DateTime]::Now.ToString([System.Globalization.DateTimeFormatInfo]::CurrentInfo.ShortTimePattern.replace(":mm",":mm:ss")) + " $line</font>") | Add-Content -Path "c:\demo\status.txt"
+        Write-Host -ForegroundColor $color $line 
+    }
+}
+
+$Folder = "C:\DOWNLOAD\VisualStudio2017Community"
+$Filename = "$Folder\vs_community.exe"
+New-Item $Folder -itemtype directory -ErrorAction ignore | Out-Null
+
+if (!(Test-Path $Filename)) {
+    Log "Downloading Visual Studio 2017 Community Setup Program"
+    $WebClient = New-Object System.Net.WebClient
+    $WebClient.DownloadFile("https://aka.ms/vs/15/release/vs_community.exe", $Filename)
+}
+
+Log "Installing Visual Studio 2017 Community (this might take a while)"
+$setupParameters = “--quiet --norestart"
+Start-Process -FilePath $Filename -WorkingDirectory $Folder -ArgumentList $setupParameters -Wait -Passthru | Out-Null
+
+$setupParameters = “--quiet --norestart --add Microsoft.VisualStudio.Component.Windows10SDK.14393"
+Start-Process -FilePath $Filename -WorkingDirectory $Folder -ArgumentList $setupParameters -Wait -Passthru | Out-Null
--- SetupWorkshop.ps1
+++ SetupWorkshop.ps1
@@ -1,26 +1,4 @@
-﻿try {
-    $Folder = "C:\DOWNLOAD\VisualStudio2017Enterprise"
-    $Filename = "$Folder\vs_enterprise.exe"
-    New-Item $Folder -itemtype directory -ErrorAction ignore | Out-Null
-    
-    if (!(Test-Path $Filename)) {
-        Log "Downloading Visual Studio 2017 Enterprise Setup Program"
-        $WebClient = New-Object System.Net.WebClient
-        $WebClient.DownloadFile("https://aka.ms/vs/15/release/vs_enterprise.exe", $Filename)
-    }
-    
-    Log "Installing Visual Studio 2017 Enterprise (this might take a while)"
-    $setupParameters = “--quiet --norestart"
-    Start-Process -FilePath $Filename -WorkingDirectory $Folder -ArgumentList $setupParameters -Wait -Passthru | Out-Null
-
-    $setupParameters = “--quiet --norestart --add Microsoft.VisualStudio.Component.Windows10SDK.14393"
-    Start-Process -FilePath $Filename -WorkingDirectory $Folder -ArgumentList $setupParameters -Wait -Passthru | Out-Null
-    
-    Start-Sleep -Seconds 10
-
-} catch {
-    Log -color Red -line ($Error[0].ToString() + " (" + ($Error[0].ScriptStackTrace -split '\r\n')[0] + ")")
-}
+﻿. (Join-Path $PSScriptRoot "Install-VS2017Community.ps1")
 
 try {
     $Folder = "C:\DOWNLOAD\AdobeReader"
