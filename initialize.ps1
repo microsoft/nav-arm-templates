50a94f988773f0dc4a6f2b1ddb17e48952ccfdf4
freddydk <freddy.kristiansen@microsoft.com>
2017-11-07 09:51:44 +0100

    use containerhelper 0.1.1.8

--- initialize.ps1
+++ initialize.ps1
@@ -215,7 +215,7 @@ Remove-Item "c:\run\my\SetupCertificate.ps1" -force
 }
 
 Log "Install Nav Container Helper from PowerShell Gallery"
-Install-Module -Name navcontainerhelper -RequiredVersion 0.1.1.5 -Force
+Install-Module -Name navcontainerhelper -RequiredVersion 0.1.1.8 -Force
 Import-Module -Name navcontainerhelper -DisableNameChecking
 
 $startupAction = New-ScheduledTaskAction -Execute "powershell.exe" -Argument $setupStartScript
