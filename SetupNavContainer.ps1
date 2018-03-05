d67d0b98865cebb8af40e54385045a378d762a49
freddydk <freddy.kristiansen@microsoft.com>
2018-03-05 03:42:21 -0800

    erroraction and download license to correct folder

--- SetupNavContainer.ps1
+++ SetupNavContainer.ps1
@@ -1,4 +1,7 @@
-﻿if (!(Test-Path function:Log)) {
+﻿$ErrorActionPreference = Stop
+$WarningActionPreference = Ignore
+
+if (!(Test-Path function:Log)) {
     function Log([string]$line, [string]$color = "Gray") {
         ("<font color=""$color"">" + [DateTime]::Now.ToString([System.Globalization.DateTimeFormatInfo]::CurrentInfo.ShortTimePattern.replace(":mm",":mm:ss")) + " $line</font>") | Add-Content -Path "c:\demo\status.txt"
         Write-Host -ForegroundColor $color $line 
