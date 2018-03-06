3a30f47a5735813238d6dfa8d1a633825ec4d376
freddydk <freddy.kristiansen@microsoft.com>
2018-03-06 01:36:03 -0800

    use navcontainerhelper 0.2.7.0

--- initialize.ps1
+++ initialize.ps1
@@ -222,7 +222,7 @@ if ($workshopFilesUrl -ne "") {
 }
 
 Log "Install Nav Container Helper from PowerShell Gallery"
-Install-Module -Name navcontainerhelper -RequiredVersion 0.2.6.9 -Force
+Install-Module -Name navcontainerhelper -RequiredVersion 0.2.7.0 -Force
 Import-Module -Name navcontainerhelper -DisableNameChecking
 
 if ($licenseFileUri -ne "") {
