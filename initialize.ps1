55a874cb8e71ff6932fd2805945f02151d19852c
freddydk <freddy.kristiansen@microsoft.com>
2018-04-05 19:29:21 +0200

    New landing page

--- initialize.ps1
+++ initialize.ps1
@@ -145,7 +145,7 @@ Download-File -sourceUrl "${scriptPath}line.png"                -destinationFile
 Download-File -sourceUrl "${scriptPath}Microsoft.png"           -destinationFile "C:\inetpub\wwwroot\Microsoft.png"
 Download-File -sourceUrl "${scriptPath}web.config"              -destinationFile "C:\inetpub\wwwroot\web.config"
 
-$title = 'Dynamics NAV Container Host'
+$title = 'Dynamics Container Host'
 [System.IO.File]::WriteAllText("C:\inetpub\wwwroot\title.txt", $title)
 [System.IO.File]::WriteAllText("C:\inetpub\wwwroot\hostname.txt", $publicDnsName)
 
@@ -212,7 +212,7 @@ if ($workshopFilesUrl -ne "") {
 }
 
 Log "Install Nav Container Helper from PowerShell Gallery"
-Install-Module -Name navcontainerhelper -RequiredVersion 0.2.7.3 -Force
+Install-Module -Name navcontainerhelper -RequiredVersion 0.2.7.5 -Force
 Import-Module -Name navcontainerhelper -DisableNameChecking
 
 if ($certificatePfxUrl -ne "" -and $certificatePfxPassword -ne "") {
