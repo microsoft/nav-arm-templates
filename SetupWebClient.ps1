02a1dcb6ac080d0545d5c9b2053faa120b5f5a01
Freddy Kristiansen <freddy.kristiansen@microsoft.com>
2018-11-16 20:55:58 +0100

    SetupWebClient to copy missing file

---
+++ SetupWebClient.ps1
@@ -0,0 +1,21 @@
+﻿. "c:\run\SetupWebClient.ps1"
+
+# Copy missing files to Web Client folder
+
+$sourcepath = (Get-Item "C:\Program Files\Microsoft Dynamics NAV\*\Web Client\WebPublish")
+if ($sourcepath) {
+    if (!(Get-Variable WebServerInstance -ErrorAction Ignore)) {
+        $WebServerInstance = "NAV"
+    }
+    $destpath = "c:\inetpub\wwwroot\$WebServerInstance"
+    
+    Get-ChildItem $sourcepath.FullName -Recurse -File | % {
+        $fp = $_.FullName
+        $df = ($destpath + $fp.Substring($sourcepath.FullName.Length))
+        if (!(Test-Path -Path $df)) {
+            Write-Host "Copy missing file $fp"
+            Copy-Item -Path $fp -Destination $df
+
+        }
+    }
+} 
