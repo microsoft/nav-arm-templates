bf56f146ceec5e421b524dac01e278960abc9478
Freddy Kristiansen <freddy.kristiansen@microsoft.com>
2018-11-26 06:27:22 +0100

    DockerAgent init

---
+++ SetupDockerAgentVM.ps1
@@ -0,0 +1,65 @@
+﻿$ErrorActionPreference = "Stop"
+$WarningActionPreference = "Continue"
+
+$ComputerInfo = Get-ComputerInfo
+$WindowsInstallationType = $ComputerInfo.WindowsInstallationType
+$WindowsProductName = $ComputerInfo.WindowsProductName
+
+try {
+
+if (Get-ScheduledTask -TaskName SetupVm -ErrorAction Ignore) {
+    schtasks /DELETE /TN SetupVm /F | Out-Null
+}
+
+function Log([string]$line) {
+    ([DateTime]::Now.ToString([System.Globalization.DateTimeFormatInfo]::CurrentInfo.ShortTimePattern.replace(":mm",":mm:ss")) + " $line") | Add-Content -Path "c:\agent\status.txt"
+}
+
+function Login-Docker([string]$registry, [string]$registryUsername, [string]$registryPassword)
+{
+    if ("$registryUsername" -ne "" -and "$registryPassword" -ne "") {
+        $securePassword = ConvertTo-SecureString -String $registryPassword -Key $passwordKey
+        $plainPassword = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto([System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($SecurePassword))
+        docker login "$registry" -u "$registryUsername" -p "$plainPassword"
+    }
+}
+
+Log "SetupDockerAgentVm, User: $env:USERNAME"
+
+. (Join-Path $PSScriptRoot "settings.ps1")
+
+Log "Starting docker"
+start-service docker
+
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Ssl3 -bor [System.Net.SecurityProtocolType]::Tls -bor [System.Net.SecurityProtocolType]::Ssl3 -bor [System.Net.SecurityProtocolType]::Tls11 -bor [System.Net.SecurityProtocolType]::Tls12
+
+Log "Enabling File Download in IE"
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\3" -Name "1803" -Value 0
+Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\3" -Name "1803" -Value 0
+
+Log "Enabling Font Download in IE"
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\3" -Name "1604" -Value 0
+Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\3" -Name "1604" -Value 0
+
+Log "Show hidden files and file types"
+Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced'  -Name "Hidden"      -value 1
+Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced'  -Name "HideFileExt" -value 0
+
+Log "Disabling Server Manager Open At Logon"
+New-ItemProperty -Path "HKCU:\Software\Microsoft\ServerManager" -Name "DoNotOpenServerManagerAtLogon" -PropertyType "DWORD" -Value "0x1" –Force | Out-Null
+
+Login-Docker -registry "$registry1" -registryUsername "$registry1username" -registryPassword "$registry1password"
+Login-Docker -registry "$registry2" -registryUsername "$registry2username" -registryPassword "$registry2password"
+Login-Docker -registry "$registry3" -registryUsername "$registry3username" -registryPassword "$registry3password"
+
+if (Get-ScheduledTask -TaskName SetupStart -ErrorAction Ignore) {
+    schtasks /DELETE /TN SetupStart /F | Out-Null
+}
+
+shutdown -r -t 30
+
+} catch {
+    Log $_.Exception.Message
+    $_.ScriptStackTrace.Replace("`r`n","`n").Split("`n") | % { Log $_ }
+    throw
+}
