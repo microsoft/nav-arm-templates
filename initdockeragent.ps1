d4ae30f85b7e02b4814ab69fa6ceafc375e9718f
Freddy Kristiansen <freddy.kristiansen@microsoft.com>
2018-11-26 04:38:26 +0100

    include registry

--- initdockeragent.ps1
+++ initdockeragent.ps1
@@ -13,7 +13,16 @@ param
     [Parameter(Mandatory=$true)]
     [string] $Queue,
     [Parameter(Mandatory=$true)]
-    [string] $vmname
+    [string] $vmname,
+    [string] $registry1 = "",
+    [string] $registry1username = "",
+    [string] $registry1password = "",
+    [string] $registry2 = "",
+    [string] $registry2username = "",
+    [string] $registry2password = "",
+    [string] $registry3 = "",
+    [string] $registry3username = "",
+    [string] $registry3password = ""
 )
 
 function Download-File([string]$sourceUrl, [string]$destinationFile)
@@ -21,6 +30,13 @@ function Download-File([string]$sourceUrl, [string]$destinationFile)
     Log "Downloading $destinationFile"
     Remove-Item -Path $destinationFile -Force -ErrorAction Ignore
     (New-Object System.Net.WebClient).DownloadFile($sourceUrl, $destinationFile)
+}
+
+function Login-Docker([string]$registry, [string]$registryUsername, [string]$registryPassword)
+{
+    if ("$registryUsername" -ne "" -and "$registryPassword" -ne "") {
+        docker login "$registry" -u "$registryUsername" -p "$registryPassword"
+    }
 }
 
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Ssl3 -bor [System.Net.SecurityProtocolType]::Tls -bor [System.Net.SecurityProtocolType]::Ssl3 -bor [System.Net.SecurityProtocolType]::Tls11 -bor [System.Net.SecurityProtocolType]::Tls12
@@ -37,6 +53,12 @@ if (!(Get-PackageProvider -Name NuGet -ListAvailable -ErrorAction Ignore)) {
 # Install Docker
 Install-module DockerMsftProvider -Force
 Install-Package -Name docker -ProviderName DockerMsftProvider -Force
+
+Start-Service docker
+
+Login-Docker -registry "$registry1" -registryUsername "$registry1username" -registryPassword "$registry1password"
+Login-Docker -registry "$registry2" -registryUsername "$registry2username" -registryPassword "$registry2password"
+Login-Docker -registry "$registry3" -registryUsername "$registry3username" -registryPassword "$registry3password"
 
 Restart-Computer -force
 
