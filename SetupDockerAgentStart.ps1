33cc54e812a5eb494a5b9c42875dde232b08b01d
Freddy Kristiansen <freddy.kristiansen@microsoft.com>
2018-11-26 06:27:22 +0100

    DockerAgent init

---
+++ SetupDockerAgentStart.ps1
@@ -0,0 +1,25 @@
+﻿function Log([string]$line) {
+    ([DateTime]::Now.ToString([System.Globalization.DateTimeFormatInfo]::CurrentInfo.ShortTimePattern.replace(":mm",":mm:ss")) + " $line") | Add-Content -Path "c:\agent\status.txt"
+}
+
+Log "SetupDockerAgentStart, User: $env:USERNAME"
+
+. (Join-Path $PSScriptRoot "settings.ps1")
+
+if (!(Get-Package -Name AzureRM.Storage -ErrorAction Ignore)) {
+    Log "Installing AzureRM.Storage PowerShell package"
+    Install-Package AzureRM.Storage -Force -WarningAction Ignore | Out-Null
+}
+
+$securePassword = ConvertTo-SecureString -String $adminPassword -Key $passwordKey
+$plainPassword = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto([System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($SecurePassword))
+
+Log "Launch SetupDockerAgentVm"
+$onceAction = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-NoProfile -WindowStyle Hidden -ExecutionPolicy UnRestricted -File c:\agent\setupDockerAgentVm.ps1"
+Register-ScheduledTask -TaskName "SetupVm" `
+                       -Action $onceAction `
+                       -RunLevel Highest `
+                       -User $vmAdminUsername `
+                       -Password $plainPassword | Out-Null
+
+Start-ScheduledTask -TaskName SetupVm
