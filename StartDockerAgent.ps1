0975d4b0f7c5eb92e6de03d50fcf975ad347d5f6
Freddy Kristiansen <freddy.kristiansen@microsoft.com>
2018-11-29 09:44:32 +0100

    transcript

--- StartDockerAgent.ps1
+++ StartDockerAgent.ps1
@@ -31,6 +31,9 @@ while ($true) {
     if (!($message)) {
         break
     }
+    $transcriptname = [Guid]::NewGuid().ToString()
+    $transcriptfilename = "c:\agent\$transcriptname.txt"
+    $transcripting = $false
     try {
         if (!(Test-Path $tempFolder -PathType Container)) {
             New-Item -ItemType Directory -Path $tempFolder | out-null
@@ -45,26 +48,40 @@ while ($true) {
                  "navversion" = $json.navversion
                  "cu" = $json.cu
                  "Country" = $json.country
-                 "Tags" = $json.tags
+                 "Dequeue" = $message.DequeueCount
                  "version" = $json.version
+                 "transcript" = ""
                }
         Add-StorageTableRow -table $table -partitionKey $AgentName -rowKey ([string]::Format("{0:D19}", [DateTime]::MaxValue.Ticks - [DateTime]::UtcNow.Ticks)) -property (@{"Status" = "Build"} + $ht) | Out-Null
+        Start-Transcript -Path $transcriptfilename
+        $transcripting = $true
         . (Join-Path $navDockerPath "$($json.task)\build.ps1") -json $json
+        $transcripting = $false
+        Stop-Transcript
+        
         Add-StorageTableRow -table $table -partitionKey $AgentName -rowKey ([string]::Format("{0:D19}", [DateTime]::MaxValue.Ticks - [DateTime]::UtcNow.Ticks)) -property (@{"Status" = "Success"} + $ht) | Out-Null
         $azureQueue.CloudQueue.DeleteMessage($message)
         . (Join-Path $navDockerPath "$($json.task)\cleanup.ps1") -Context $storageContext -json $json
     } catch {
+        if ($transcripting) {
+            Stop-Transcript
+        }
+
+        Set-AzureStorageBlobContent -File $transcriptfilename -Context $storageContext -Container $json.blobcontainer -Blob $transcriptname -Force | Out-Null
+        $ht.transcript = "https://$StorageAccountName.blob.core.windows.net/$($json.blobContainer)/$transcriptname"
+        
         if ($message.DequeueCount -eq 10) {
-            Add-StorageTableRow -table $table -partitionKey $AgentName -rowKey ([string]::Format("{0:D19}", [DateTime]::MaxValue.Ticks - [DateTime]::UtcNow.Ticks)) -property (@{"Status" = "Error"} + $ht) | Out-Null
+            Add-StorageTableRow -table $table -partitionKey $AgentName -rowKey ([string]::Format("{0:D19}", [DateTime]::MaxValue.Ticks - [DateTime]::UtcNow.Ticks)) -property (@{"Status" = "Fail"} + $ht) | Out-Null
             $azureQueue.CloudQueue.DeleteMessage($message)
             . (Join-Path $navDockerPath "$($json.task)\cleanup.ps1") -Context $storageContext -json $json
         } else {
-            Add-StorageTableRow -table $table -partitionKey $AgentName -rowKey ([string]::Format("{0:D19}", [DateTime]::MaxValue.Ticks - [DateTime]::UtcNow.Ticks)) -property (@{"Status" = "Warning $($message.DequeueCount)"} + $ht) | Out-Null
+            Add-StorageTableRow -table $table -partitionKey $AgentName -rowKey ([string]::Format("{0:D19}", [DateTime]::MaxValue.Ticks - [DateTime]::UtcNow.Ticks)) -property (@{"Status" = "Retry"} + $ht) | Out-Null
             Start-Sleep -Seconds 60
         }
     } finally {
         Remove-Item -Path $tempFolder -Recurse -Force -ErrorAction Ignore
         Remove-Item -Path $tempFile -Force -ErrorAction Ignore
+        Remove-Item -Path $transcriptfilename -Force -ErrorAction Ignore
     }
 }
 
