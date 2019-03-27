85bd447770fb46ba2a3d18d107a720923d6b82eb
Freddy Kristiansen <freddy.kristiansen@microsoft.com>
2019-03-27 19:38:23 +0100

    additional-install.ps1

---
+++ additional-install.ps1
@@ -0,0 +1,2 @@
+﻿(New-Object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/Microsoft/nav-arm-templates/dev/additional-install.cmd', 'c:\demo\additional-install.cmd')
+& C:\demo\additional-install.cmd
