ffd608a25d14c77543fe358ab55f934264aad01c
Ron Koppelaar <Ron.Koppelaar@cegeka-dsa.nl>
2019-05-15 12:52:11 +0200

    Let logging not block the creation of the container

--- SetupNavContainer.ps1
+++ SetupNavContainer.ps1
@@ -1,6 +1,6 @@
 ﻿if (!(Test-Path function:Log)) {
     function Log([string]$line, [string]$color = "Gray") {
-        ("<font color=""$color"">" + [DateTime]::Now.ToString([System.Globalization.DateTimeFormatInfo]::CurrentInfo.ShortTimePattern.replace(":mm",":mm:ss")) + " $line</font>") | Add-Content -Path "c:\demo\status.txt"
+        ("<font color=""$color"">" + [DateTime]::Now.ToString([System.Globalization.DateTimeFormatInfo]::CurrentInfo.ShortTimePattern.replace(":mm",":mm:ss")) + " $line</font>") | Add-Content -Path "c:\demo\status.txt" -ErrorAction SilentlyContinue
         Write-Host -ForegroundColor $color $line 
     }
 }
