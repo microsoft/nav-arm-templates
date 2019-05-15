f3efd8d93066250be5d9bf8353bcc66cff024333
Ron Koppelaar <Ron.Koppelaar@cegeka-dsa.nl>
2019-05-15 12:53:44 +0200

    Let logging not block the creation of the container

--- SetupVm.ps1
+++ SetupVm.ps1
@@ -12,7 +12,7 @@ if (Get-ScheduledTask -TaskName SetupVm -ErrorAction Ignore) {
 }
 
 function Log([string]$line, [string]$color = "Gray") {
-    ("<font color=""$color"">" + [DateTime]::Now.ToString([System.Globalization.DateTimeFormatInfo]::CurrentInfo.ShortTimePattern.replace(":mm",":mm:ss")) + " $line</font>") | Add-Content -Path "c:\demo\status.txt" 
+    ("<font color=""$color"">" + [DateTime]::Now.ToString([System.Globalization.DateTimeFormatInfo]::CurrentInfo.ShortTimePattern.replace(":mm",":mm:ss")) + " $line</font>") | Add-Content -Path "c:\demo\status.txt" -ErrorAction SilentlyContinue
 }
 
 Log "SetupVm, User: $env:USERNAME"
