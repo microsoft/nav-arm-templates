24d38f939bdca2ac0f9c9044448ed257735fe938
Freddy Kristiansen <freddy.kristiansen@microsoft.com>
2019-05-21 10:09:57 +0200

    Use Saas configuration

---
+++ Saas/SetupConfiguration.ps1
@@ -0,0 +1,20 @@
+﻿. "C:\RUN\SetupConfiguration.ps1"
+
+$SaasSettings = @{
+    "ODataServicesMaxPageSize" = "20000"
+    "ClientServicesMaxUploadSize" = "150"
+    "ClientServicesMaxItemsInObjectGraph" = "512"
+}
+
+$SaasSettings.Keys | % {
+    $setting = $customConfig.SelectSingleNode("//appSettings/add[@key='$_']")
+    if ($setting) {
+        Write-Host "Setting $_ to $($SaasSettings[$_])"
+        $setting.Value = $SaasSettings[$_]
+    }
+    else {
+        Write-Host "Ignoring setting $_"
+    }
+}
+
+$CustomConfig.Save($CustomConfigFile)
