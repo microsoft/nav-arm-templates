c3d3521cb91d4ad64b9bee27e9b812e23e48f8f0
Freddy Kristiansen <freddyk@microsoft.com>
2020-07-28 21:27:31 +0200

    Add nchBranch variable

--- initialize.ps1
+++ initialize.ps1
@@ -144,6 +144,7 @@ if (Test-Path $settingsScript) {
     Get-VariableDeclaration -name "RequestToken"           | Add-Content $settingsScript
     Get-VariableDeclaration -name "CreateStorageQueue"     | Add-Content $settingsScript
     Get-VariableDeclaration -name "AddTraefik"             | Add-Content $settingsScript
+    Get-VariableDeclaration -name "nchBranch"              | Add-Content $settingsScript
 
     $passwordKey = New-Object Byte[] 16
     [Security.Cryptography.RNGCryptoServiceProvider]::Create().GetBytes($passwordKey)
