5c5848007174e7296685976103b2152046b7e2f4
Ron Koppelaar <Ron.Koppelaar@cegeka-dsa.nl>
2020-09-07 17:06:47 +0200

    Publish the 16.4 app to BLOB storage

--- SetupNavContainer.ps1
+++ SetupNavContainer.ps1
@@ -375,7 +375,8 @@ if ($auth -eq "AAD") {
     else {
         $appfile = Join-Path $env:TEMP "AzureAdAppSetup.app"
         if (([System.Version]$navVersion) -ge ([System.Version]"16.4.14693.15445")) {
-            Download-File -sourceUrl "http://aka.ms/Microsoft_AzureAdAppSetup_16.4.app" -destinationFile $appfile
+           #Download-File -sourceUrl "http://aka.ms/Microsoft_AzureAdAppSetup_16.4.app" -destinationFile $appfile
+           Download-File -sourceUrl "https://erpsources.blob.core.windows.net/azure-ad-app-setup/Microsoft_AzureAdAppSetup_16.4.app" -destinationFile $appfile
         }
         elseif (([System.Version]$navVersion).Major -ge 15) {
             #Download-File -sourceUrl "http://aka.ms/Microsoft_AzureAdAppSetup_15.0.app" -destinationFile $appfile
