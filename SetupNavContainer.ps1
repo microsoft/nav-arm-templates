66a48d59f5c6af41b649ca5db3e77388cc1e569e
Ron Koppelaar <Ron.Koppelaar@cegeka-dsa.nl>
2020-09-07 13:23:44 +0200

    Use default AzureADSetup from MS starting with 16.4

--- SetupNavContainer.ps1
+++ SetupNavContainer.ps1
@@ -374,7 +374,10 @@ if ($auth -eq "AAD") {
     } 
     else {
         $appfile = Join-Path $env:TEMP "AzureAdAppSetup.app"
-        if (([System.Version]$navVersion).Major -ge 15) {
+        if (([System.Version]$navVersion) -ge ([System.Version]"16.4.14693.15445")) {
+            Download-File -sourceUrl "http://aka.ms/Microsoft_AzureAdAppSetup_16.4.app" -destinationFile $appfile
+        }
+        elseif (([System.Version]$navVersion).Major -ge 15) {
             #Download-File -sourceUrl "http://aka.ms/Microsoft_AzureAdAppSetup_15.0.app" -destinationFile $appfile
             #CDSA: Download app from our own storage with dependency to cegeka-dsa BaseApp
             Download-File -sourceUrl "https://erpsources.blob.core.windows.net/azure-ad-app-setup/Microsoft_AzureAdAppSetup_15.0.0.0.app" -destinationFile $appfile
