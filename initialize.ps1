b6775082d73c779fb9d33ae2a06a26a71dfb5901
bogdanurse <41284730+bogdanurse@users.noreply.github.com>
2021-06-28 12:41:31 +0300

    Revert "temporary fix (#19)" (#22)
    This reverts commit f886e97da7a03170cb88526187b9d203ef94f6bd.

--- initialize.ps1
+++ initialize.ps1
@@ -51,7 +51,7 @@ param
        [string] $requestToken              = "",
        [string] $createStorageQueue        = "",
        [string] $AddTraefik                = "No",
-       [string] $nchBranch                 = "preview"
+       [string] $nchBranch                 = ""
 )
 
 $verbosePreference = "SilentlyContinue"
