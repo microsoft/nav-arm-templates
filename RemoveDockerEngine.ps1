56711aa02a5e6533c5bb60cdeab86be556398d58
Freddy Kristiansen <freddyk@microsoft.com>
2021-10-23 23:46:05 +0200

    remove

---
+++ RemoveDockerEngine.ps1
@@ -0,0 +1,15 @@
+﻿# Stop and Remove docker service
+$dockerService = get-service docker -ErrorAction SilentlyContinue
+if ($dockerService) {
+    if ($dockerService.Status -eq "Running") {
+        Stop-Service docker
+    }
+    $service = Get-WmiObject -Class Win32_Service -Filter "Name='docker'"
+    $service.Delete()
+}
+
+# Remove installation folder
+Remove-Item -path $env:ProgramFiles\docker -Recurse -Force
+
+# Remove docker in ProgramData
+# Remove docker data-root
