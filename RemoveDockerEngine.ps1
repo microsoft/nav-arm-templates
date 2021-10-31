ed16ab62cee5904c098bf12e04d2d5eb2305d12e
Freddy Kristiansen <freddyk@microsoft.com>
2021-10-31 10:08:02 +0100

    remove path

--- RemoveDockerEngine.ps1
+++ RemoveDockerEngine.ps1
@@ -1,19 +1,15 @@
 ﻿# Stop and Remove docker service
-dockerd --unregister-service
-Start-Sleep -Seconds 30
-
 $dockerService = get-service docker -ErrorAction SilentlyContinue
 if ($dockerService) {
     if ($dockerService.Status -eq "Running") {
         Stop-Service docker
     }
-    $service = Get-WmiObject -Class Win32_Service -Filter "Name='docker'"
-    $service.Delete()
+    dockerd --unregister-service
 }
 
 $path = [System.Environment]::GetEnvironmentVariable("Path", "User")
-if (";$path;" -notlike "*;$($env:ProgramFiles)\docker;*") {
-    [Environment]::SetEnvironmentVariable("Path", "$path;" -replace ";C:\\Program files\\docker;", ";", [System.EnvironmentVariableTarget]::User)
+if (";$path;" -like "*;$($env:ProgramFiles)\docker;*") {
+    [Environment]::SetEnvironmentVariable("Path", ("$path;" -replace ";C:\\Program files\\docker;", ";"), [System.EnvironmentVariableTarget]::User)
 }
 
 # Remove installation folder
