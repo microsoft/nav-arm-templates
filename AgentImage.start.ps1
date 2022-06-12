c190026c23fb389de2c016afff014bd362bbb0ce
freddydk <freddyk@microsoft.com>
2022-06-12 08:35:29 +0200

    support docker agents

---
+++ AgentImage.start.ps1
@@ -0,0 +1,32 @@
+$agentFolder = "c:\agent"
+if ("$ENV:AGENTURL" -eq "" -or "$ENV:ORGANIZATION" -eq "" -or "$ENV:AGENTNAME" -eq "" -or "$ENV:TOKEN" -eq "" -or "$ENV:POOL" -eq "") {
+    Write-Host "You need to specify the following Environment variables in order to run the agent image`n"
+    Write-Host "AGENTURL - the URL for downloading the Agent. GitHub runner URL can be found at https://github.com/{organization}/{repository}/settings/actions/runners/new (ex. https://github.com/actions/runner/releases/download/v2.284.0/actions-runner-win-x64-2.284.0.zip). x64 Azure DevOps Agent can be found at https://dev.azure.com/{your_organization}/_admin/_AgentPool (click Download agent and select x64 ) (ex. https://vstsagentpackage.azureedge.net/agent/2.194.0/vsts-agent-win-x64-2.194.0.zip)"
+    Write-Host "ORGANIZATION - the URL for your GitHub Project/Organization or your Azure DevOps Organization (ex. https://github.com/BusinessCentralApps, https://github.com/freddydk/BingMaps.AppSource or https://dev.azure.com/freddykristiansen)"
+    Write-Host "AGENTNAME - the name of the Agent"
+    Write-Host "TOKEN - a personal access token with permissions to add/remove agents from the agent pool for Azure DevOps or the token provided at https://github.com/{organization}/{repository}/settings/actions/runners/new for Github"
+    Write-Host "POOL - additional labels for GitHub runners or specify the pool in which your agent should live for Azure DevOps"
+}
+elseif (Test-Path (Join-Path $agentFolder 'run.cmd')) {
+    Set-Location $agentFolder
+    .\run.cmd
+}
+else {
+
+    New-Item -Path c:\ProgramData\BcContainerHelper -ItemType Directory | Out-Null
+    Copy-Item -Path c:\bcch\bccontainerhelper.config.json C:\ProgramData\BcContainerHelper -Force
+    $agentZip = "c:\agent.zip"
+    (New-Object System.Net.WebClient).DownloadFile($ENV:AGENTURL, $agentZip)
+    New-Item -Path $agentFolder -ItemType Directory | Out-Null
+    Set-Location $agentFolder
+    Add-Type -AssemblyName System.IO.Compression.FileSystem
+    [System.IO.Compression.ZipFile]::ExtractToDirectory($agentZip, $agentFolder)
+
+    if ($ENV:AGENTURL -like 'https://github.com/actions/runner/releases/download/*') {
+        .\config.cmd --unattended --url "$ENV:ORGANIZATION" --token "$ENV:TOKEN" --name $ENV:AGENTNAME --labels "$ENV:POOL" --windowslogonaccount "NT AUTHORITY\SYSTEM"
+    }
+    else {
+        .\config.cmd --unattended --url "$ENV:ORGANIZATION" --auth PAT --token "$ENV:TOKEN" --agent $ENV:AGENTNAME --pool "$ENV:POOL" --windowslogonaccount "NT AUTHORITY\SYSTEM"
+    }
+    .\run.cmd
+}
--- initbuildagent.ps1
+++ initbuildagent.ps1
@@ -19,7 +19,8 @@ param
     [Parameter(Mandatory=$true)]
     [int] $count = 1,
     [Parameter(Mandatory=$true)]
-    [string] $vmname
+    [string] $vmname,
+    [bool] $runInsideDocker = $true
 )
 
 function Download-File([string]$sourceUrl, [string]$destinationFile)
@@ -47,7 +48,7 @@ if ($installDocker) {
     $installDockerScriptUrl = $templateLink.Substring(0,$templateLink.LastIndexOf('/')+1)+'InstallOrUpdateDockerEngine.ps1'
     $installDockerScript = Join-Path $DownloadFolder "InstallOrUpdateDockerEngine.ps1"
     Download-File -sourceUrl $installDockerScriptUrl -destinationFile $installDockerScript
-    . $installDockerScript -Force -envScope "Machine"
+    . $installDockerScript -Force -envScope "Machine" -dataRoot 'c:\d'
 }
 
 if ($finalSetupScriptUrl) {
@@ -60,22 +61,63 @@ if ($finalSetupScriptUrl) {
     . $finalSetupScript
 }
 
-Add-Type -AssemblyName System.IO.Compression.FileSystem
-$agentFilename = $agentUrl.Substring($agentUrl.LastIndexOf('/')+1)
-$agentFullname = Join-Path $DownloadFolder $agentFilename
-Download-File -sourceUrl $agentUrl -destinationFile $agentFullname
-1..$count | ForEach-Object {
-    $agentName = "$vmName-$_-$([guid]::NewGuid().ToString())"
-    $agentFolder = "C:\$_"
-    mkdir $agentFolder -ErrorAction Ignore | Out-Null
-    Set-Location $agentFolder
-    [System.IO.Compression.ZipFile]::ExtractToDirectory($agentFullname, $agentFolder)
+if ($runInsideDocker) {
+    Set-Location $DownloadFolder
+    New-Item -Path 'image' -ItemType Directory | Out-Null
+    Set-Location 'image'
 
-    if ($agentUrl -like 'https://github.com/actions/runner/releases/download/*') {
-        .\config.cmd --unattended --url "$organization" --token "$token" --name $agentName --labels "$pool" --runAsService --windowslogonaccount "NT AUTHORITY\SYSTEM"
+    $startScriptUrl = $templateLink.Substring(0,$templateLink.LastIndexOf('/')+1)+'AgentImage.start.ps1'
+    Download-File -sourceUrl $startScriptUrl -destinationFile 'start.ps1'
+    $dockerFileUrl = $templateLink.Substring(0,$templateLink.LastIndexOf('/')+1)+'AgentImage.DOCKERFILE'
+    Download-File -sourceUrl $dockerFileUrl -destinationFile 'DOCKERFILE'
+
+    $os = (Get-CimInstance Win32_OperatingSystem)
+    $UBR = (Get-ItemProperty 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion' -Name UBR).UBR
+    $hostOsVersion = [System.Version]::Parse("$($os.Version).$UBR")
+    if ($os.version -eq '10.0.22000') {
+        $hostOsVersion = 'ltsc2022'
     }
-    else {
-        .\config.cmd --unattended --url "$organization" --auth PAT --token "$token" --pool "$pool" --agent $agentName --runAsService --windowslogonaccount "NT AUTHORITY\SYSTEM"
+    $serverCoreImage = "mcr.microsoft.com/windows/servercore:$($hostOsVersion)"
+    $imageName = 'runneragent:latest'
+    docker build --build-arg baseimage=$serverCoreImage --tag $imageName .
+
+    1..$count | ForEach-Object {
+        $agentContainerName = "agent$_"
+
+        $agentName = "$vmName-docker-$_-$([guid]::NewGuid().ToString())"
+        
+        $bcartifactsCacheVolumeName = 'bcartifacts.cache'
+        $bcContainerHelperVolumeName = 'hostHelperFolder'
+        $AgentWorkVolumeName = "$($agentContainerName)_Work"
+        
+        $allVolumes = "{$(((docker volume ls --format "'{{.Name}}': '{{.Mountpoint}}'") -join ",").Replace('\','\\').Replace("'",'"'))}" | ConvertFrom-Json | ConvertTo-HashTable
+        $bcartifactsCacheVolumeName, $bcContainerHelperVolumeName, $AgentWorkVolumeName | ForEach-Object {
+            if (-not $allVolumes.ContainsKey($_)) { docker volume create $_ }
+        }
+        $allVolumes = (docker volume ls --format "{ '{{.Name}}': '{{.Mountpoint}}' }").Replace('\','\\').Replace("'",'"') | ConvertFrom-Json | ConvertTo-HashTable
+        @{ "useVolumes" = $true; "ContainerHelperFolder" = "c:\bcch"; "defaultNewContainerParameters" = @{ "isolation" = "hyperv" } } | ConvertTo-Json -Depth 99 | Set-Content (Join-Path $allVolumes.hosthelperfolder "BcContainerHelper.config.json") -Encoding UTF8
+        
+        docker run -d --name $agentContainerName -v \\.\pipe\docker_engine:\\.\pipe\docker_engine -v C:\ProgramData\docker\volumes:C:\ProgramData\docker\volumes --mount source=$bcartifactsCacheVolumeName,target=c:\bcartifacts.cache --mount source=$bcContainerHelperVolumeName,target=C:\BCCH --mount source=$AgentWorkVolumeName,target=C:\Sources --env AGENTURL=$agentUrl --env ORGANIZATIOn=$organization --env AGENTNAME=$agentName --env POOL=$pool --env TOKEN=$token $imageName
+    }
+}
+else {
+    Add-Type -AssemblyName System.IO.Compression.FileSystem
+    $agentFilename = $agentUrl.Substring($agentUrl.LastIndexOf('/')+1)
+    $agentFullname = Join-Path $DownloadFolder $agentFilename
+    Download-File -sourceUrl $agentUrl -destinationFile $agentFullname
+    1..$count | ForEach-Object {
+        $agentName = "$vmName-$_-$([guid]::NewGuid().ToString())"
+        $agentFolder = "C:\$_"
+        mkdir $agentFolder -ErrorAction Ignore | Out-Null
+        Set-Location $agentFolder
+        [System.IO.Compression.ZipFile]::ExtractToDirectory($agentFullname, $agentFolder)
+    
+        if ($agentUrl -like 'https://github.com/actions/runner/releases/download/*') {
+            .\config.cmd --unattended --url "$organization" --token "$token" --name $agentName --labels "$pool" --runAsService --windowslogonaccount "NT AUTHORITY\SYSTEM"
+        }
+        else {
+            .\config.cmd --unattended --url "$organization" --auth PAT --token "$token" --pool "$pool" --agent $agentName --runAsService --windowslogonaccount "NT AUTHORITY\SYSTEM"
+        }
     }
 }
 
