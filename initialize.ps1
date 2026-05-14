d31e502d7a6347db76eda18e64443b6b4a7e42d4
Maria Zhelezova <43066499+mazhelez@users.noreply.github.com>
2026-05-14 20:33:12 +0200

    Add authentication to web status page

--- initialize.ps1
+++ initialize.ps1
@@ -219,9 +219,9 @@ if (!(Get-Module powershellget | Where-Object { $_.Version -ge [version]"2.2.5" 
 
 AddToStatus "Installing Internet Information Server (this might take a few minutes)"
 if ($WindowsInstallationType -eq "Server") {
-    Add-WindowsFeature Web-Server,web-Asp-Net45
+    Add-WindowsFeature Web-Server,web-Asp-Net45,Web-Basic-Auth
 } else {
-    Enable-WindowsOptionalFeature -Online -FeatureName IIS-WebServer,IIS-ASPNET45 -All -NoRestart | Out-Null
+    Enable-WindowsOptionalFeature -Online -FeatureName IIS-WebServer,IIS-ASPNET45,IIS-BasicAuthentication -All -NoRestart | Out-Null
 }
 
 Remove-Item -Path "C:\inetpub\wwwroot\iisstart.*" -Force
